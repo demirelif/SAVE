40f5f4d65c60f402fcec2ec6d1e646c6bc5479f5
elif.demir <ed.demirelif@gmail.com>
2020-12-27 01:13:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/saveandroid/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.saveandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
