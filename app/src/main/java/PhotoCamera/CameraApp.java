0418797f448c19f655b32bb392cf8852116287dd
elif.demir <ed.demirelif@gmail.com>
2021-02-22 23:23:34 +0300

    getting front and back facing cameras

--- app/src/main/java/PhotoCamera/CameraApp.java
+++ app/src/main/java/PhotoCamera/CameraApp.java
@@ -2,10 +2,26 @@ package PhotoCamera;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.*;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.MediaRecorder;
+import android.util.Log;
+import android.view.TextureView;
+import android.widget.Toast;
+
+import com.example.saveandroid.R;
+
+import java.util.concurrent.TimeUnit;
 
 public class CameraApp {
+
+    public static final String CAMERA_FRONT = "1";
+    public static final String CAMERA_BACK = "0";
+
+    private String cameraOn = CAMERA_BACK;
+    private TextureView mTextureView;
 
     public void startCameras(Activity activity){
         CameraManager manager = (CameraManager) activity.getSystemService(Context.CAMERA_SERVICE);
@@ -20,5 +36,43 @@ public class CameraApp {
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    private String getFrontFacingCameraId(CameraManager cManager) {
+        try {
+            String cameraId;
+            int cameraOrientation;
+            CameraCharacteristics characteristics;
+            for (int i = 0; i < cManager.getCameraIdList().length; i++) {
+                cameraId = cManager.getCameraIdList()[i];
+                characteristics = cManager.getCameraCharacteristics(cameraId);
+                cameraOrientation = characteristics.get(CameraCharacteristics.LENS_FACING);
+                if (cameraOrientation == CameraCharacteristics.LENS_FACING_FRONT) {
+                    return cameraId;
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getBackFacingCameraId(CameraManager cManager) {
+        try {
+            String cameraId;
+            int cameraOrientation;
+            CameraCharacteristics characteristics;
+            for (int i = 0; i < cManager.getCameraIdList().length; i++) {
+                cameraId = cManager.getCameraIdList()[i];
+                characteristics = cManager.getCameraCharacteristics(cameraId);
+                cameraOrientation = characteristics.get(CameraCharacteristics.LENS_FACING);
+                if (cameraOrientation == CameraCharacteristics.LENS_FACING_BACK) {
+                    return cameraId;
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
